987fd18272ee817486631ed038f73bc0dc01d22a
jstzwj <1103870790@qq.com>
2023-06-22 03:12:06 +0800

    add a hifigan example

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # vtbaudio
-State-of-the-art Audio Machine Learning Models for Pytorch and TensorFlow
+State-of-the-art Audio Machine Learning Models.
+
+## Usage
+
+Use HifiGAN to convert mel to wav:
+```python
+import torchaudio
+import torchaudio.transforms as T
+
+# Load audio
+wav, sr = torchaudio.load("zszy_48k.wav")
+assert sr == 48000
+
+from vtbaudio.pipelines import MelPipeline
+audio_pipeline = MelPipeline(freq=48000, n_fft=2048, n_mel=128, win_length=2048, hop_length=512)
+
+from vtbaudio.models.vocoders.hifigan.configuration_hifigan import HifiGANConfig
+from vtbaudio.models.vocoders.hifigan.modeling_hifigan import HifiGAN, HifiGANPipeline
+
+hifigan_48k = HifiGAN.from_pretrained("vtb-plan/hifigan-48k")
+
+mel = audio_pipeline(wav)
+out = hifigan_48k(mel)
+
+```
--- hifigan_example.py
+++ hifigan_example.py
@@ -1,3 +1,24 @@
 import vtbaudio
 from vtbaudio.models.vocoders.hifigan.configuration_hifigan import HifiGANConfig
-from vtbaudio.models.vocoders.hifigan.modeling_hifigan import HifiGAN
+from vtbaudio.models.vocoders.hifigan.modeling_hifigan import HifiGAN, HifiGANPipeline
+from vtbaudio.pipelines import MelPipeline
+
+hifigan_48k = HifiGAN.from_pretrained("vtb-plan/hifigan-48k")
+
+import torchaudio
+import torchaudio.transforms as T
+
+# Load audio
+wav, sr = torchaudio.load("zszy_48k.wav")
+assert sr == 48000
+
+# mel = mel_spectrogram_torch(wav, 2048, 128, 48000, 512, 2048, 0, None, False)
+audio_pipeline = MelPipeline(freq=48000,
+                                n_fft=2048,
+                                n_mel=128,
+                                win_length=2048,
+                                hop_length=512)
+mel = audio_pipeline(wav)
+out = hifigan_48k(mel)
+
+# get output wav
