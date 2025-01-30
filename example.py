c33fc892c37c6b018be662f77a5df0180f1f2cd1
jstzwj <1103870790@qq.com>
2025-01-30 13:37:26 +0800

    update

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
 import deepaudio
 from deepaudio.models.vocoders.hifigan.configuration_hifigan import HifiGANConfig
-from deepaudio.models.vocoders.hifigan.modeling_hifigan import HifiGANGenerator
+from deepaudio.models.vocoders.hifigan.modeling_hifigan import HifiGAN
 
